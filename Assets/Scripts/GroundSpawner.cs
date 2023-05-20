31528e0ace85c2d38e542413bd4f5d0418f9aa81
Patryk Garwol <patrykgarwol2000@wp.pl>
2023-05-20 23:33:58 +0200

    formatting time after game

--- Assets/Scripts/GroundSpawner.cs
+++ Assets/Scripts/GroundSpawner.cs
@@ -6,10 +6,12 @@ public class GroundSpawner : MonoBehaviour {
     public GameObject groundTile;
     Vector3 nextSpawnPoint = new Vector3(0, 0, 0.98f);
 
+    int raceTrackLength = 3; // default: 3
+
     void Start() {
-       for (int i = 1; i <= 3; i++) {
+       for (int i = 1; i <= raceTrackLength; i++) {
             SpawnTile();
-            if (i == 1)//2
+            if (i == raceTrackLength - 1)//2
                 groundTile = GameObject.Find("FINALRaceTrackSegment");
 
         }
